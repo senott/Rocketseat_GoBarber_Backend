2ff16b70c9b12b47b20695b1af38a81486d5f47a
Senott <senott@yahoo.com>
2020-08-24 11:45:03 -0300

    Appointments router.

--- src/server.ts
+++ src/server.ts
@@ -4,6 +4,10 @@ import routes from './routes';
 
 const app = express();
 
+app.use(express.json());
+
+app.use(routes);
+
 app.get('/', (request, response) => response.json({ message: 'Hello World' }));
 
 app.listen(3333, () => {

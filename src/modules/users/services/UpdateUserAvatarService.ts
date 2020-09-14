39b487ed84b368c15e7389499a7287267f4f3741
Senott <senott@yahoo.com>
2020-09-14 11:47:37 -0300

    Base url and paths configured.

--- src/modules/users/services/UpdateUserAvatarService.ts
+++ src/modules/users/services/UpdateUserAvatarService.ts
@@ -4,9 +4,9 @@ import { getRepository } from 'typeorm';
 import fs from 'fs';
 import path from 'path';
 
-import User from '../models/User';
-import uploadConfig from '../config/upload';
-import AppError from '../errors/AppError';
+import User from '@modules/users/infra/typeorm/entities/User';
+import uploadConfig from '@config/upload';
+import AppError from '@shared/errors/AppError';
 
 interface Request {
   user_id: string;

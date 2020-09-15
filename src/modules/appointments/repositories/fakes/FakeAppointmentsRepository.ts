dfc3fdaf4e8fabca008df3ea74b1e94c89b8e334
Senott <senott@yahoo.com>
2020-09-15 18:20:50 -0300

    Tests for CreateAppointmentService.

---
+++ src/modules/appointments/repositories/fakes/FakeAppointmentsRepository.ts
@@ -0,0 +1,39 @@
+/* eslint-disable camelcase */
+import { uuid } from 'uuidv4';
+import { isEqual } from 'date-fns';
+
+import Appointment from '@modules/appointments/infra/typeorm/entities/Appointment';
+
+import IAppointmentsRepository from '@modules/appointments/repositories/IAppointmentsRepository';
+import ICreateAppointmentDto from '@modules/appointments/dtos/ICreateAppointmentDTO';
+
+class AppointmentsRepository implements IAppointmentsRepository {
+  private appointments: Appointment[] = [];
+
+  public async findByDate(date: Date): Promise<Appointment | undefined> {
+    const appointment = this.appointments.find(item =>
+      isEqual(item.date, date),
+    );
+
+    return appointment;
+  }
+
+  public async create({
+    provider_id,
+    date,
+  }: ICreateAppointmentDto): Promise<Appointment> {
+    const appointment = new Appointment();
+
+    Object.assign(appointment, {
+      id: uuid(),
+      date,
+      provider_id,
+    });
+
+    this.appointments.push(appointment);
+
+    return appointment;
+  }
+}
+
+export default AppointmentsRepository;
--- src/modules/appointments/services/CreateAppointmenteService.spec.ts
+++ src/modules/appointments/services/CreateAppointmenteService.spec.ts
@@ -1,3 +1,42 @@
-test('Sum two numbers', () => {
-  expect(1 + 2).toBe(3);
+import AppError from '@shared/errors/AppError';
+import FakeAppointmentsRepository from '../repositories/fakes/FakeAppointmentsRepository';
+import CreateAppointmentService from './CreateAppointmentService';
+
+describe('CreateAppointment', () => {
+  it('should be able to create new appointment', async () => {
+    const fakeAppointmentsRepository = new FakeAppointmentsRepository();
+
+    const createAppointment = new CreateAppointmentService(
+      fakeAppointmentsRepository,
+    );
+
+    const appointment = await createAppointment.execute({
+      date: new Date(),
+      provider_id: '12345678',
+    });
+
+    expect(appointment).toHaveProperty('id');
+  });
+
+  it('should not be able to create 2 appointments at same time', async () => {
+    const fakeAppointmentsRepository = new FakeAppointmentsRepository();
+
+    const createAppointment = new CreateAppointmentService(
+      fakeAppointmentsRepository,
+    );
+
+    const date = new Date();
+
+    await createAppointment.execute({
+      date,
+      provider_id: '12345678',
+    });
+
+    expect(
+      createAppointment.execute({
+        date,
+        provider_id: '12345678',
+      }),
+    ).rejects.toBeInstanceOf(AppError);
+  });
 });

150a6b87c1bada210d2e044a19c4c50d103299be
Senott <senott@yahoo.com>
2020-09-21 15:26:21 -0300

    Send forgot password email service and tests.

---
+++ src/modules/users/repositories/IUserTokensRepository.ts
@@ -0,0 +1,6 @@
+import UserToken from '../infra/typeorm/entities/UserToken';
+
+export default interface IUserTokensRepository {
+  // eslint-disable-next-line camelcase
+  generate(user_id: string): Promise<UserToken>;
+}

ca5020b5b498665a713a5bd9091591ece2b1d9a8
Senott <senott@yahoo.com>
2020-09-22 10:25:45 -0300

    User tokens repository, forgot password and reset password controllers.

---
+++ src/modules/users/infra/typeorm/repositories/UserTokensRepository.ts
@@ -0,0 +1,29 @@
+/* eslint-disable camelcase */
+import { getRepository, Repository } from 'typeorm';
+import UserToken from '@modules/users/infra/typeorm/entities/UserToken';
+
+import IUserTokensRepository from '@modules/users/repositories/IUserTokensRepository';
+
+class UserTokensRepository implements IUserTokensRepository {
+  private ormRepository: Repository<UserToken>;
+
+  constructor() {
+    this.ormRepository = getRepository(UserToken);
+  }
+
+  async findByToken(token: string): Promise<UserToken | undefined> {
+    const userToken = await this.ormRepository.findOne({ where: token });
+
+    return userToken;
+  }
+
+  public async generate(user_id: string): Promise<UserToken> {
+    const userToken = this.ormRepository.create({ user_id });
+
+    await this.ormRepository.save(userToken);
+
+    return userToken;
+  }
+}
+
+export default UserTokensRepository;

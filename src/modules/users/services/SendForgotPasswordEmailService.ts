c1c5788042d7ca558709eeaa8f9e15e7db7aec30
Senott <senott@yahoo.com>
2020-09-22 18:35:19 -0300

    Forgot password e-mail template.

--- src/modules/users/services/SendForgotPasswordEmailService.ts
+++ src/modules/users/services/SendForgotPasswordEmailService.ts
@@ -1,5 +1,6 @@
 /* eslint-disable no-useless-constructor */
 import { injectable, inject } from 'tsyringe';
+import path from 'path';
 
 import AppError from '@shared/errors/AppError';
 
@@ -32,12 +33,22 @@ class SendForgotPasswordEmailService {
 
     const { token } = await this.userTokensRepository.generate(user.id);
 
+    const forgotPasswordTemplate = path.resolve(
+      __dirname,
+      '..',
+      'views',
+      'forgot_password.hbs',
+    );
+
     await this.mailProvider.sendMail({
       to: { name: user.name, email: user.email },
       subject: '[GoBarber] Recuperação de Senha',
       templateData: {
-        template: 'Olá, {{name}}: {{token}}',
-        variables: { name: user.name, token },
+        file: forgotPasswordTemplate,
+        variables: {
+          name: user.name,
+          link: `http://localhost:3000/reset_password?token=${token}`,
+        },
       },
     });
   }

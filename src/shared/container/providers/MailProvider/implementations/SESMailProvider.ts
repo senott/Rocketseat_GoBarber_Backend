1898d89432cf98f27b89a313cdf0205b704b419d
Senott <senott@yahoo.com>
2020-09-28 18:12:05 -0300

    Amazon SES for production emails.

---
+++ src/shared/container/providers/MailProvider/implementations/SESMailProvider.ts
@@ -0,0 +1,45 @@
+import nodemailer, { Transporter } from 'nodemailer';
+import { injectable, inject } from 'tsyringe';
+import aws from 'aws-sdk';
+
+import mailConfig from '@config/mail';
+import IMailTemplateProvider from '../../MailTemplateProvider/models/IMailTemplateProvider';
+
+import ISendMailDTO from '../dtos/ISendMailDTO';
+import IMailProvider from '../models/IMailProvider';
+
+@injectable()
+export default class SESMailProvider implements IMailProvider {
+  private client: Transporter;
+
+  constructor(
+    @inject('MailTemplateProvider')
+    private mailTemplateProvider: IMailTemplateProvider,
+  ) {
+    this.client = nodemailer.createTransport({
+      SES: new aws.SES({
+        apiVersion: '2010-12-01',
+        region: 'sa-east-1',
+      }),
+    });
+  }
+
+  public async sendMail({
+    to,
+    from,
+    subject,
+    templateData,
+  }: ISendMailDTO): Promise<void> {
+    const config = mailConfig.defaults.from;
+
+    await this.client.sendMail({
+      from: {
+        name: from?.name || config.name,
+        address: from?.email || config.email,
+      },
+      to: { name: to.name, address: to.email },
+      subject,
+      html: await this.mailTemplateProvider.parse(templateData),
+    });
+  }
+}
